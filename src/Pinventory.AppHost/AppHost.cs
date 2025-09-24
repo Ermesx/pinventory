c89c25bb7eaf4a3f510deea2c44b27fff88a83f8
Wojciech Gadziski <wojtekgadzinski@gmail.com>
2025-09-24 22:24:41 +0200

    Add api project and basic dependecies

--- src/Pinventory.AppHost/AppHost.cs
+++ src/Pinventory.AppHost/AppHost.cs
@@ -1,3 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var databse = builder.AddPostgres("database")
+    .WithPgAdmin()
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithDataVolume()
+    .AddDatabase("pinventory-db");
+
+builder.AddProject<Projects.Pinventory_Api>("pinventory-api")
+    .WithReference(databse)
+    .WaitFor(databse);
+
 builder.Build().Run();
