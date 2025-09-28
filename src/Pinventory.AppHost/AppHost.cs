ca221ebe544d13ea8f60952b75d481a0a36e1110
Wojciech Gadziski <wojtekgadzinski@gmail.com>
2025-09-28 22:11:55 +0200

    Add services sctructure

--- src/Pinventory.AppHost/AppHost.cs
+++ src/Pinventory.AppHost/AppHost.cs
@@ -1,30 +1,68 @@
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Pinventory_DataSync_Worker>("pinventory-datasync-worker");
+var rabbitMq = builder.AddRabbitMQ("rabbit-mq")
+    .WithManagementPlugin()
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithDataVolume();
 
-builder.AddProject<Projects.Pinventory_Notifications_Api>("pinventory-notifications-api");
-
-builder.AddProject<Projects.Pinventory_Pins_Api>("pinventory-pins-api");
-
-builder.AddProject<Projects.Pinventory_Taging_Worker>("pinventory-taging-worker");
-
-builder.AddProject<Projects.Pinventory_Api>("pinventory-api");
-
-builder.AddProject<Projects.Pinventory_Identity_Api>("pinventory-identity-api");
-
-
-
-IResourceBuilder<PostgresDatabaseResource> identityDatabase = builder.AddPostgres("database")
+var identityDatabase = builder.AddPostgres("identity-db")
     .WithPgAdmin()
     .WithLifetime(ContainerLifetime.Persistent)
     .WithDataVolume()
     .AddDatabase("pinventory-identity-db");
 
-builder.AddProject<Projects.Pinventory_Identity_Api>("pinventory-identity-api")
+var identityApi =  builder.AddProject<Projects.Pinventory_Identity_Api>("pinventory-identity-api")
     .WithReference(identityDatabase)
-    .WaitFor(identityDatabase);
+    .WithReference(rabbitMq)
+    .WaitFor(identityDatabase)
+    .WaitFor(rabbitMq);
 
+var notificationDatabase = builder.AddPostgres("notification-db")
+    .WithPgAdmin()
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithDataVolume()
+    .AddDatabase("pinventory-notification-db");
 
-builder.AddProject<Projects.Pinventory_Web>("pinventory-web");
+var notificationsApi = builder.AddProject<Projects.Pinventory_Notifications_Api>("pinventory-notifications-api")
+    .WithReference(notificationDatabase)
+    .WithReference(rabbitMq)
+    .WaitFor(notificationDatabase)
+    .WaitFor(rabbitMq);
+
+var pinsDatabase = builder.AddPostgres("pins-db")
+    .WithPgAdmin()
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithDataVolume()
+    .AddDatabase("pinventory-pins-db");
+
+var pinventoryApi = builder.AddProject<Projects.Pinventory_Pins_Api>("pinventory-pins-api")
+    .WithReference(pinsDatabase)
+    .WithReference(rabbitMq)
+    .WaitFor(pinsDatabase)
+    .WaitFor(rabbitMq);
+
+builder.AddProject<Projects.Pinventory_DataSync_Worker>("pinventory-datasync-worker")
+    .WithReference(pinsDatabase)
+    .WithReference(rabbitMq)
+    .WaitFor(pinsDatabase)
+    .WaitFor(rabbitMq);
+
+builder.AddProject<Projects.Pinventory_Taging_Worker>("pinventory-taging-worker")
+    .WithReference(pinsDatabase)
+    .WithReference(rabbitMq)
+    .WaitFor(pinsDatabase)
+    .WaitFor(rabbitMq);
+
+var api = builder.AddProject<Projects.Pinventory_Api>("pinventory-api")
+    .WithReference(identityApi)
+    .WithReference(notificationsApi)
+    .WithReference(pinventoryApi)
+    .WaitFor(identityApi)
+    .WaitFor(notificationsApi)
+    .WaitFor(pinventoryApi);
+
+builder.AddProject<Projects.Pinventory_Web>("pinventory-web")
+    .WithReference(api)
+    .WaitFor(api);
 
 builder.Build().Run();
