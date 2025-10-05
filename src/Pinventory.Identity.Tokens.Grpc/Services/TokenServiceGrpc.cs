41d601060a5548810911cecd561bec751e55c8a3
Wojciech Gadziski <wojtekgadzinski@gmail.com>
2025-10-06 01:14:34 +0200

    add grpc tokens endpoints

---
+++ src/Pinventory.Identity.Tokens.Grpc/Services/TokenServiceGrpc.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Claims;
+
+using Google.Protobuf.WellKnownTypes;
+
+using Grpc.Core;
+
+namespace Pinventory.Identity.Tokens.Grpc.Services;
+
+public class TokenServiceGrpc(TokenService service) : Tokens.TokensBase
+{
+    private static RpcException NotFound => new(new Status(StatusCode.NotFound, "User or tokens not found"));
+    
+    public override async Task<TokenResponse> GetAccessToken(UserRequest request, ServerCallContext context)
+    {
+        var principal = CreatePrincipal(request.UserId);
+        var tokens = await service.GetGoogleTokensAsync(principal);
+
+        return tokens is not null
+            ? CreateTokenResponse(tokens.AccessToken)
+            : throw NotFound;
+    }
+
+    public override async Task<TokenResponse> RefreshAccessToken(UserRequest request, ServerCallContext context)
+    {
+        var principal = CreatePrincipal(request.UserId);
+        var token = await service.RefreshGoogleAccessTokenAsync(principal, context.CancellationToken);
+        return token is not null
+            ? CreateTokenResponse(token)
+            : throw NotFound;
+    }
+    
+    private static TokenResponse CreateTokenResponse(GoogleAccessToken token) =>
+        new() { AccessToken = token.Token, ExpiresAt = token.ExpiresAt.ToTimestamp() };
+
+    private static ClaimsPrincipal CreatePrincipal(string userId) =>
+        new(new ClaimsIdentity([new(ClaimTypes.NameIdentifier, userId)]));
+}
