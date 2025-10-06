555421d9a1bd574b215c909acae2cca8534e19fc
Wojciech Gadziski <wojtekgadzinski@gmail.com>
2025-10-06 22:20:14 +0200

    Remove token refreshing - google can do it itself

--- src/Pinventory.Identity.Tokens.Grpc/Services/TokenServiceGrpc.cs
+++ src/Pinventory.Identity.Tokens.Grpc/Services/TokenServiceGrpc.cs
@@ -19,15 +19,6 @@ public class TokenServiceGrpc(TokenService service) : Tokens.TokensBase
             ? CreateTokenResponse(tokens.AccessToken)
             : throw NotFound;
     }
-
-    public override async Task<TokenResponse> RefreshAccessToken(UserRequest request, ServerCallContext context)
-    {
-        var principal = CreatePrincipal(request.UserId);
-        var token = await service.RefreshGoogleAccessTokenAsync(principal, context.CancellationToken);
-        return token is not null
-            ? CreateTokenResponse(token)
-            : throw NotFound;
-    }
     
     private static TokenResponse CreateTokenResponse(GoogleAccessToken token) =>
         new() { AccessToken = token.Token, ExpiresAt = token.ExpiresAt.ToTimestamp() };
