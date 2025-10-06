cdb299dcc65efa076eb8e540445f7321d9bceec8
Wojciech Gadziski <wojtekgadzinski@gmail.com>
2025-10-06 16:37:08 +0200

    Add test projects

---
+++ tests/Pinventory.Pins.Api.IntegrationTests/WebAppFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Pinventory.Pins.Api.IntegrationTests;
+
+
+public sealed class PinsApiWebAppFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        // TODO: Swap EF Core connection string to Postgres Testcontainer.
+        // TODO: Configure OpenTelemetry as no-op for tests.
+        // TODO: Replace external auth handlers with a test scheme.
+    }
+}
+
