4473c13c9df61cae0ea207b02a2a980803f48b2f
Wojciech Gadziski <wojtekgadzinski@gmail.com>
2025-10-08 18:11:57 +0200

    add import service

---
+++ src/Pinventory.Pins.Import.Worker/DataPortability/IImportService.cs
@@ -0,0 +1,7 @@
+﻿namespace Pinventory.Pins.Import.Worker.DataPortability;
+
+public interface IImportService
+{
+    Task<string> InitiateDataArchive(Period? period);
+    Task<DataArchiveState> CheckDataArchive(string archiveJobId);
+}
