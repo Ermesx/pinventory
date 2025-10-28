e10282228f41012e6fa9313032a2b2b08c58ba6f
Wojciech Gadziski <wojtekgadzinski@gmail.com>
2025-10-29 00:02:26 +0100

    Add logging to `AdminAuthorizationService` for missing Google user ID detection

--- src/Pinventory.Web/Authorization/AdminAuthorizationService.cs
+++ src/Pinventory.Web/Authorization/AdminAuthorizationService.cs
@@ -5,12 +5,21 @@ using Pinventory.Web.Google;
 
 namespace Pinventory.Web.Authorization;
 
-public class AdminAuthorizationService(IGoogleUserService googleUserService, IOptions<PinventoryOptions> options)
+public class AdminAuthorizationService(
+    IGoogleUserService googleUserService,
+    IOptions<PinventoryOptions> options,
+    ILogger<AdminAuthorizationService> logger)
     : IAdminAuthorizationService
 {
     public async Task<bool> IsCurrentUserAdminAsync()
     {
         var googleUserId = await googleUserService.GetGoogleUserIdAsync();
+        if (googleUserId is null)
+        {
+            logger.LogWarning("No Google user ID found for current user");
+            return false;
+        }
+
         return googleUserId == options.Value.AdminId;
     }
 }
